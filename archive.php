244a923bd845c207775e2dde9c21589e3f43ed54
AudioVoyeur <pieterbrink@gmail.com>
2017-10-05 21:47:27 +0200

    Move page navi to index.php
    Address issue when Wordpress is set to summery - the page navi then displays below each snippet rather than below all snippets.

--- archive.php
+++ archive.php
@@ -8,6 +8,7 @@
 
 				<?php // Edit the loop in /templates/archive-loop. Or roll your own. ?>
 				<?php get_template_part( 'templates/archive', 'loop'); ?>
+				<?php get_template_part( 'templates/post-navigation'); ?>
 
 			</main>
 
